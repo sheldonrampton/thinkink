028d5140fd2d04d2c5f73e2e82e5868947fd45e7
Sheldon Rampton <sheldon@sheldonrampton.com>
2012-01-08 10:24:55 -0600

    more theme updates

---
+++ thinkink/sites/all/themes/ninesixty/views-slideshow-controls-text-pause.tpl.php
@@ -0,0 +1,1 @@
+<span id="views_slideshow_controls_text_pause_<?php print $variables['vss_id']; ?>" class="<?php print $classes; ?>"><a href="#"><img src="/sites/all/themes/ninesixty/images/pause.png" alt="Pause" /></a></span>
---
+++ thinkink/sites/all/themes/ninesixty/views-slideshow-controls-text-previous.tpl.php
@@ -0,0 +1,1 @@
+<span id="views_slideshow_controls_text_previous_<?php print $variables['vss_id']; ?>" class="<?php print $classes; ?>"><a href="#"><img src="/sites/all/themes/ninesixty/images/prev.png" alt="Previous" /></a></span>
